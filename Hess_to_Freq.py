2d6118096e0af49f95c77a38fc3ae90a9710881d
Robert Paton <robertpaton@gmail.com>
2017-04-13 22:20:50 +0100

    substantially better comments

--- Hess_to_Freq.py
+++ Hess_to_Freq.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python
 
-# THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
 # Comments and/or additions are welcome (send e-mail to:
 # robert.paton@chem.ox.ac.uk
 
@@ -15,78 +7,63 @@
 #          Hess_to_Freq.py          #
 #####################################
 ###  Written by:  Rob Paton #########
-###  Last modified:  Mar 27, 2016 ###
+###  Last modified:  Mar 27, 2017 ###
 #####################################
 
 import sys, math
 import numpy as np
 
-# PHYSICAL CONSTANTS
-GAS_CONSTANT = 8.3144621
-PLANCK_CONSTANT = 6.62606957e-34 
-BOLTZMANN_CONSTANT = 1.3806488e-23 
-SPEED_OF_LIGHT = 2.99792458e10
-AVOGADRO_CONSTANT = 6.0221415e23
-AMU_to_KG = 1.66053886E-27
-autokcal = 627.509541
-kjtokcal = 4.184
-atmos = 101.325
-
-def read_hess(file, freq_scale_factor,iso):
-
-   # Force constants are scaled by freq_scale_factor squared
-   force_const_scale_factor = freq_scale_factor ** 2
+def read_hess(file, iso):
+   # The force constant matrix is read from g09 ouptut
+   # The matrix values are mass-weighted according to the isotopic masses
+   # Vibrational scaling factors are not applied to matrix elements at this stage:
+   # the resulting frequencies can be scaled after diagonalization
 
    # Read gaussian output
    g09_output = open(file, 'r')
-   
+
    inlines = g09_output.readlines()
-   
+
    mass_list = []
    start_force = len(inlines) < 100
    for i in range(0,len(inlines)):
       if inlines[i].strip().find('and mass') > -1:
          mass_list.append(float(inlines[i].strip().split()[8]))
 
-      if inlines[i].strip().startswith('Deg. of freedom'):
-         d_o_f = int(inlines[i].strip().split()[3]) + 6
-
-      if inlines[i].strip().startswith('The second derivative matrix:'):  start_hess = i + 2
-      if inlines[i].strip().startswith('ITU='): end_hess = i
+      if inlines[i].strip().startswith('NAtoms='):
+         d_o_f = int(inlines[i].strip().split()[1])*3
 
       if inlines[i].strip().find('NImag') > -1: start_force = i
 
+   # Hessian and mass-weighted Hessian (3N x 3N matrices, N = no. atoms)
    hess_mat = np.ndarray(shape=(d_o_f,d_o_f))
+   mw_hess_mat = np.ndarray(shape=(d_o_f,d_o_f))
 
+   # Isotopic substitution will consider 1H/2H, 12C/13C and 16O/17O. More can be
+   # added, but it hasn't been necessary so far...
    isolist = iso.split()
    for i in range(0,len(mass_list)):
       for atom in isolist:
          if i == int(atom)-1:
-            #print "increasing the mass of atom", iso 
-            if mass_list[i] == 1.00783: mass_list[i] = 2.0141
-            if mass_list[i] == 12.00000: mass_list[i] = 13.00335
-            if mass_list[i] == 15.99491: mass_list[i] = 16.9991
-
+            if mass_list[i] == 1.00783: mass_list[i] = 2.0141 # protium - deuterium
+            if mass_list[i] == 12.00000: mass_list[i] = 13.00335 # 12C - 13C
+            if mass_list[i] == 15.99491: mass_list[i] = 16.9991 # 16O - 17O
    longline = ""
    for i in range(start_force,len(inlines)):
       longline = longline + inlines[i].rstrip().lstrip()
-   
+
    forces = longline.split("NImag")[1].split('\\')[2].split(',')
-   
    n = 0; l = -1
    for m in range (0, d_o_f):
       for n in range(0,m+1):
          l = l + 1
-         #print m, n, forces[l],
          Hmn = forces[l]
+         sqrt_Mmn = (mass_list[m/3] * mass_list[n/3]) ** 0.5
+         hess_mat[n,m] = float(Hmn)
+         mw_hess_mat[m,n] = ((float(Hmn) / sqrt_Mmn))
+         mw_hess_mat[n,m] = ((float(Hmn) / sqrt_Mmn))
 
-         sqrt_Mmn = mass_list[m/3] * mass_list[n/3] 
-         sqrt_Mmn = sqrt_Mmn ** 0.5
-         GDF = 435.942 / ( 5.29167 * 5.29167 )
-         hess_mat[m,n] = GDF * ((float(Hmn) / sqrt_Mmn))
-         hess_mat[n,m] = GDF * ((float(Hmn) / sqrt_Mmn))
-
-   return hess_mat
+   return mw_hess_mat
 
 if __name__ == "__main__":
    # Takes arguments: g09_output_files and optional temperature and vibrational scaling factor
@@ -107,15 +84,8 @@ if __name__ == "__main__":
       sys.exit()
 
    for file in files:
-      SPEED_OF_LIGHT = 2.99792458e10
-      AVOGADRO_CONSTANT = 6.0221415e23
-      AMU_to_KG = 1.66053886E-27
-      CNV = 5.8890E-07
-
-      mw_hessmat = read_hess(file, freq_scale_factor, 0)
-      #print (mw_hessmat)
+      mw_hessmat = read_hess(file, 0)
       eigs = np.linalg.eigvalsh(mw_hessmat)
-      for eig in eigs: 
-         if eig > 0: print (eig / CNV) ** 0.5 
-         else: print -1 * (-eig/ CNV) ** 0.5 
-
+      for eig in eigs:
+         if eig > 0: print eig ** 0.5
+         else: print -1 * -eig ** 0.5
--- Kinisot.py
+++ Kinisot.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python
 
-# THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
 # Comments and/or additions are welcome (send e-mail to:
 # robert.paton@chem.ox.ac.uk
 
@@ -18,167 +10,150 @@
 ###  Last modified:  Mar 27, 2016 ###
 #####################################
 
-import sys, math
+import sys, math, time
 import numpy as np
+from glob import glob
 from Hess_to_Freq import *
 
 # PHYSICAL CONSTANTS
-GAS_CONSTANT = 8.3144621
 PLANCK_CONSTANT = 6.62606957e-34
 BOLTZMANN_CONSTANT = 1.3806488e-23
 SPEED_OF_LIGHT = 2.99792458e10
-AVOGADRO_CONSTANT = 6.0221415e23
-AMU_to_KG = 1.66053886E-27
-autokcal = 627.509541
-kjtokcal = 4.184
-atmos = 101.325
+Eh = 4.359745e-18
+a0 = 5.291772e-11
+amu = 1.660468e-27
 
-# vibrational energy evaluation (depends on frequencies, temp and scaling factor)
-def calc_vibrational_energy(frequency_wn, temperature,freq_scale_factor):
+def calc_product_factor(frequency_wn, freq_scale_factor):
    """
-   Calculates the vibrational energy contribution (kcal/mol)
-   Includes ZPE (0K) and thermal contributions
-   Evib = R * Sum(0.5 hv/k + 1/(e^(hv/KT)-1))
+   Calculates the product of (scaled) vibrational frequencies in order to
+   obtain the Teller-Redlich product factor. There is no temperture dependence
+   to this term in the B-M equation
    """
-   energy = 1.0
+   product = 1.0
+   for freq in frequency_wn:
+      product = product * freq * freq_scale_factor
+   return product
+
+def calc_zpe_factor(frequency_wn, temperature, freq_scale_factor):
+   """
+   Calculates the vibrational ZPE of (scaled) vibrational frequencies. ZPEs
+   themselves are not temperature dependent although the exponential form of
+   this term in the B-M equation is.
+   """
+   product = 1.0
+   frequency = [entry * SPEED_OF_LIGHT for entry in frequency_wn]
+   for entry in frequency:
+      hv_over_kt = ((PLANCK_CONSTANT*entry*freq_scale_factor)/(BOLTZMANN_CONSTANT * temperature))
+      product = product * math.exp(0.5 * hv_over_kt)
+   return product
+
+def calc_excitation_factor(frequency_wn, temperature, freq_scale_factor):
+   """
+   Calculates the excitation factor term of the RPFR from the (scaled) vibrational
+   frequencies. This term is temperature dependent.
+   """
+   product = 1.0
    frequency = [entry * SPEED_OF_LIGHT for entry in frequency_wn]
    for entry in frequency:
       factor = ((PLANCK_CONSTANT*entry*freq_scale_factor)/(BOLTZMANN_CONSTANT*temperature))
-      energy = energy * factor
-   return energy
+      product = product * (1-math.exp(-factor))
+   return product
 
-
-# vibrational zero point energy evaluation (depends on frequencies and scaling factor)
-def calc_zeropoint_energy(frequency_wn,freq_scale_factor):
-   """
-   Calculates the vibrational ZPE (kcal/mol)
-   EZPE = Sum(0.5 hv/k)
-   """
-   energy = 0.0
-   frequency = [entry * SPEED_OF_LIGHT for entry in frequency_wn]
-   for entry in frequency:
-      factor = ((PLANCK_CONSTANT*entry*freq_scale_factor)/(BOLTZMANN_CONSTANT))
-      temp = 0.5*factor
-      temp = temp*GAS_CONSTANT
-      energy = energy + temp
-   energy = energy/kjtokcal/1000.0
-   return energy
-
-
-# rigid-rotor harmonic oscillator entropy evaluation
-def calc_harmonic_entropy(frequency_wn, temperature,freq_scale_factor):
-   """
-   Calculates the entropic contribution (cal/(mol*K)) of a harmonic oscillator for
-   a list of frequencies of vibrational modes
-   Sv = R(hv/(k(e^(hv/KT)-1) - ln(1-e^(-hv/kT)))   
-   """
-   entropy = 1.0
-   frequency = [entry * SPEED_OF_LIGHT for entry in frequency_wn]
-   for entry in frequency:
-      factor = ((PLANCK_CONSTANT*entry*freq_scale_factor)/(BOLTZMANN_CONSTANT*temperature))
-      temp = 1-math.exp(-factor)
-      entropy = entropy * temp
-   return entropy
-
-
-class calc_rpfr:   
-   def __init__(self, file, temperature, freq_scale_factor,isomer):
-
+class calc_rpfr:
+   #Computes the Reduced Isotopic Partition Function Ratio from a structure and a given isotopic substitution
+   def __init__(self, file, isomer, *args):
       # Frequencies in waveunmbers
       frequency_wn = []
-      
-      # Read commandline arguments
-      g09_output = open(file, 'r')
-   
-      mw_hessmat = read_hess(file, freq_scale_factor,isomer)
-      eigs = np.linalg.eigvalsh(mw_hessmat)
-      for eig in eigs:
-         if eig > 0.00147: frequency_wn.append((eig / 5.8890E-07) ** 0.5)
-         if eig < -0.00147: self.im_frequency_wn = (-1.0 * eig / 5.8890E-07) ** 0.5
-      
-      #print frequency_wn
-   
-      # Calculate vibrational contributions to the energy
-      Uvib = calc_vibrational_energy(frequency_wn, temperature,freq_scale_factor)
-      ZPE = calc_zeropoint_energy(frequency_wn, freq_scale_factor)
-      # Calculate harmonic entropy for each frequency - functions defined above
-      Svib = calc_harmonic_entropy(frequency_wn, temperature,freq_scale_factor)
-      
-      # Collect all terms 
-      self.enthalpy = Uvib/autokcal
-      self.zpe = ZPE/autokcal
-      self.entropy = Svib/autokcal/1000.0
-      self.gibbs_free_energy = self.enthalpy - temperature * self.entropy
-   
+
+      # Extract the Force constants from a g09 logfile and generate the
+      # mass-weighted Hessian matrix
+      mw_hessmat = read_hess(file, isomer)
+
+      # Convert from atomic units - a bit ugly
+      conv_factor = Eh/a0**2/amu
+      eigs = np.linalg.eigvalsh(mw_hessmat*conv_factor)
+      fac = 5.30883671512e-12
+      freqs = [ np.copysign(np.sqrt(np.abs(freq)),freq) * fac for freq in eigs ]
+
+      # Keep frequencies larger in magnitude than the predefined cut-off
+      # This should remove 5 or 6 normal modes (linear or non-linear molecule)
+      # Add sanity check for removal?
+      for freq in freqs:
+         if np.abs(freq) > freq_cutoff:
+             if freq > 0.0: frequency_wn.append(freq)
+             else:  self.im_frequency_wn = -1.0 * freq
+
+      # Calculate the excitation factor (EXC), the ZPE (ZPE) and Teller-Redlich product factor (PF)
+      self.PF = calc_product_factor(frequency_wn, freq_scale_factor)
+      self.ZPE = calc_zpe_factor(frequency_wn, temperature, freq_scale_factor)
+      self.EXC = calc_excitation_factor(frequency_wn, temperature, freq_scale_factor)
+
 if __name__ == "__main__":
-   
    # Takes arguments: g09_output_files and optional temperature and vibrational scaling factor
    files = []
-   temperature = "none"; freq_scale_factor = "none"
+   # Default values
+   temperature = 298.15; freq_scale_factor = 1.0; freq_cutoff = 50.0
 
    if len(sys.argv) > 1:
       for i in range(1,len(sys.argv)):
          if sys.argv[i] == "-t": temperature = float(sys.argv[i+1])
          elif sys.argv[i] == "-s": freq_scale_factor = float(sys.argv[i+1])
          elif sys.argv[i] == "-iso": label = (sys.argv[i+1])
+         elif sys.argv[i] == "-cutoff": freq_cutoff = (sys.argv[i+1])
          else:
             if len(sys.argv[i].split(".")) > 1:
-               if sys.argv[i].split(".")[1] == "out" or sys.argv[i].split(".")[1] == "log": files.append(sys.argv[i])
-         
-      if temperature != "none": print "   Temperature =", temperature, "K;",
-      else: print "   Temperature (default) = 298.15K;",; temperature = 298.15
-      if freq_scale_factor != "none": print "   Frequency scale factor =", freq_scale_factor
-      else: print "   Frequency scale factor (default) = 1.0"; freq_scale_factor = 1.0
-      
-   else:
-      print "\nWrong number of arguments used. Correct format: Kinisot.py (-t temp) (-s scalefactor) 2 x g09_output_files\n"
-      sys.exit()
-   
-   print "\n  ",
-   print "Structure".ljust(60), "Im Freq".rjust(10), "   ZPE".rjust(11)
-   Standard = []
-   beta = 1000.0 * autokcal * kjtokcal / GAS_CONSTANT / temperature
+               if sys.argv[i].split(".")[1] == "out" or sys.argv[i].split(".")[1] == "log":
+                   for file in glob(sys.argv[i]): files.append(file)
 
-   isotopomers = ["0",label]
+      print "   KINISOT.py v 1.0.1", time.strftime("%Y-%m-%d %H:%M")
+      print "   Temperature =", temperature, "K;   Frequency scale factor =", freq_scale_factor
+      #print "   Frequency cut-off =", freq_cutoff, "wavenumbers"
+
+   else:
+      print "\nWrong number of arguments used. Correct format: Kinisot.py 2 x g09_output_files -iso 1 2 3 (-t <temp>) (-s <scalefactor>) (-cutoff <value>)\n"
+      sys.exit()
+
+   print "\n  ",
+   print "Structure".ljust(32), "Imag. Freq (cm-1)".rjust(10)
+
+   # Conversion from wavenumbers to SI energy units; then divide by kT
+   tofreq = SPEED_OF_LIGHT * PLANCK_CONSTANT / BOLTZMANN_CONSTANT / temperature
    KIE = []
 
-   for iso in isotopomers:
+   # Calculates the RPFR for each for the reactant and its isotopomer and then the transition structure and the same isotopomer
+   for iso in ["0",label]:
       for file in files:
-         rpfr = calc_rpfr(file, temperature, freq_scale_factor,iso)
+         rpfr = calc_rpfr(file, iso, temperature, freq_scale_factor, freq_cutoff)
          print "o ",
-         print (file.split(".")[0]+": label @ "+str(iso)).ljust(60),
-         if hasattr(rpfr, "im_frequency_wn"): print "%.6f" % (rpfr.im_frequency_wn),
-         else: print "N/A".rjust(10),
-         if hasattr(rpfr, "zpe"): print "   %.6f" % (rpfr.zpe)
-         else: print "N/A".rjust(10)
+         print file.split(".")[0],
+         if iso != "0": print "- isotopic @", iso,
+         if hasattr(rpfr, "im_frequency_wn"): print '{:27.2f}'.format(rpfr.im_frequency_wn)
+         else: print "------".rjust(26)
          KIE.append(rpfr)
-      
-   
-   print "\n  ","".ljust(30), "V-ratio".rjust(10), "  ZPE-ratio".rjust(11), "   Exc".rjust(11), "   TR-Prod".rjust(11), "   KIE".rjust(11), "   Tunn".rjust(11), "   corr-KIE".rjust(11)
+
+   # Fancy printing
+   print "\n  ","".ljust(30), "V-ratio".rjust(10), "  ZPE".rjust(11), "   EXC".rjust(11), "   TRPF".rjust(11), "   KIE".rjust(11), "   1D-tunn".rjust(11), "   corr-KIE".rjust(11)
    print ("   TST-KIE @ "+str(temperature)+" K").ljust(30),
 
    if hasattr(KIE[1], "im_frequency_wn") and hasattr(KIE[3], "im_frequency_wn"):
       imfreq_fac = KIE[1].im_frequency_wn/KIE[3].im_frequency_wn
       print "     %.6f" % (imfreq_fac),
-   
-      tofreq = SPEED_OF_LIGHT * PLANCK_CONSTANT / BOLTZMANN_CONSTANT / temperature
-   
-      # A correction factor for QM-tunneling (infinite parabola)
+
+      # A correction factor for QM-tunneling (Bell infinite parabola)
       parabolic_tunn_corr = imfreq_fac * math.sin(0.5 * tofreq * KIE[3].im_frequency_wn) / math.sin(0.5 * tofreq * KIE[1].im_frequency_wn)
    else:
-      print "o  Require two transition structures with imaginary frequencies!"
-      sys.exit()      
+      print "o  Kinisot requires a transition structure with an imaginary frequencies!"
+      sys.exit()
 
+   # Application of the Bigeleisen-Mayer equation
+   # https://en.wikipedia.org/wiki/Maria_Goeppert-Mayer
    # Compute separately the rate factors associated with differences in ZPE, vibrational entropy and then enthalpy...
-   zpe_fac = math.exp(beta*((KIE[0].zpe-KIE[2].zpe)-(KIE[1].zpe-KIE[3].zpe)))
-   print "   %.6f" % (zpe_fac),
-   exp_fac = (KIE[0].entropy/KIE[2].entropy) / (KIE[1].entropy/KIE[3].entropy)     
-   print "   %.6f" % (exp_fac),
-   freq_fac = (KIE[1].enthalpy/KIE[3].enthalpy) / (KIE[0].enthalpy/KIE[2].enthalpy)
-   print "   %.6f" % (freq_fac),
-   
-   # print out the KIE from TST with classical nuclei and then a value corrected to include quantum tunneling effects...
-   kie_tot = imfreq_fac * zpe_fac * exp_fac * freq_fac
-   corr_kie_tot = kie_tot * parabolic_tunn_corr
-   print "   %.6f" % (kie_tot), "   %.6f" % (parabolic_tunn_corr), "   %.6f" % (corr_kie_tot)
+   ZPE = (KIE[0].ZPE/KIE[2].ZPE) / (KIE[1].ZPE/KIE[3].ZPE)
+   EXC = (KIE[0].EXC/KIE[2].EXC) / (KIE[1].EXC/KIE[3].EXC)
+   TRPF = (KIE[1].PF/KIE[3].PF) / (KIE[0].PF/KIE[2].PF)
+   print "   %.6f" % (ZPE), "   %.6f" % (EXC), "   %.6f" % (TRPF),
 
+   # print out (a) the Bigeleisen-Mayer KIE with classical nuclei and then (b) a value corrected to include quantum tunneling effects...
+   KIE = imfreq_fac * ZPE * EXC * TRPF
+   KIE_Tunnel = KIE * parabolic_tunn_corr
+   print "   %.6f" % (KIE), "   %.6f" % (parabolic_tunn_corr), "   %.6f" % (KIE_Tunnel)
