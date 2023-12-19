8274977965d3b471926b677a19d46ba92c7aa167
Robert Paton <bobbypaton@users.noreply.github.com>
2023-12-19 00:31:15 +0000

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+
+try:
+    import goodvibes
+    BASEPATH = os.path.join(kinisot.__path__[0])
+except ImportError:
+    here = os.path.dirname(os.path.abspath(__file__))
+    BASEPATH = os.path.normpath(os.path.join(here, '..', 'kinisot'))
+
+
+def datapath(path):
+    return os.path.join(BASEPATH, 'examples', path)
